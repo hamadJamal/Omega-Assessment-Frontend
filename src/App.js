c90b817108f9e94df335656c5435601e870c36d4
Muhammad Hamad Jamal <hamad.jamal@datainvent.com>
2025-03-01 00:28:44 +0500

    Feature: Protected Route Introduced

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import GrammarPage from "./pages/GrammarPage";
 import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -10,7 +11,14 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/grammar" element={<GrammarPage />} />
+          <Route
+            path="/grammar"
+            element={
+              <ProtectedRoute>
+                <GrammarPage />
+              </ProtectedRoute>
+            }
+          />
           <Route path="*" element={<LoginPage />} />
         </Routes>
       </Router>
